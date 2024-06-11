5112ab006b1e6285d4171e4b90e6a17d59e1e51a
MaciejChacinski <164795475+MaciejChacinski@users.noreply.github.com>
2024-06-11 23:04:00 +0200

    Update Pandemia.java

--- CODE/Pandemia/src/Pandemia/Pandemia.java
+++ CODE/Pandemia/src/Pandemia/Pandemia.java
@@ -1,37 +1,54 @@
 package Pandemia;
 
-import java.awt.BorderLayout;
-
-
-import java.awt.Color;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import java.util.ArrayList;
-
 import javax.swing.*;
-
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
-public class Pandemia extends JFrame implements ActionListener{
+public class Pandemia extends JFrame implements ActionListener {
 
-    JMenuItem newItem, saveItem, openItem, jezykItem, autorzyItem, polskiItem, angielskiItem, runItem;
+    JMenuItem jezykItem, autorzyItem, polskiItem, angielskiItem, runItem, changeBackgroundColorItem;
     VirusPanel virusPanel;
     JPanel pandemiaPanel;
     JMenu SubMenu, Menu;
     JMenuBar menuBar;
-    private int numberOfPeople; 
-    private Locale currentLocale = new Locale("pl", "PL");
     
-    ArrayList<Virus> viruses = new ArrayList<Virus>();
     
     private CFrame simulationPanel;
     
     
-    public Pandemia() 
-    {
+    private int numberOfPeople;
+    private Locale currentLocale = new Locale("pl", "PL");
+
+    // Stałe tłumaczeń
+    private static final String TITLE_KEY = "SymulacjaPandemii";
+    private static final String MENU_KEY = "Menu";
+    private static final String LANGUAGE_KEY = "Jezyk";
+    private static final String POLISH_KEY = "Polski";
+    private static final String ENGLISH_KEY = "Angielski";
+    private static final String AUTHORS_KEY = "Autorzy";
+    private static final String VIRUS_MENU_KEY = "WybierzWirusa";
+    private static final String VIRUS_COLOR_KEY = "KolorWirusa";
+    private static final String HEALTHY_COLOR_KEY = "KolorZdrowego";
+    private static final String IMMUNE_COLOR_KEY = "KolorUodpornionego";
+    private static final String DEAD_COLOR_KEY = "KolorZmarlego";
+    private static final String SAVE_KEY = "Zapisz";
+    private static final String OPEN_KEY = "Otworz";
+    private static final String LIST_KEY = "Lista";
+    private static final String RUN_KEY = "Run";
+    private static final String NUMBER_LABEL_KEY = "NumberLabel";
+    private static final String PARAMETERS_KEY = "Parametry";
+    private static final String SPREADABILITY_KEY = "spred";
+    private static final String COMPLEXITY_KEY = "complex";
+    private static final String LETHALITY_KEY = "leth";
+    private static final String CHANGE_BACKGROUND_COLOR_KEY = "ZmienKolorTla";
+    private static final String STOP = "zatrzymajSymulacje";
+
+
+
+    public Pandemia() {
         setTitle("Symulacja pandemii");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(new BorderLayout());
@@ -42,172 +59,121 @@ public class Pandemia extends JFrame implements ActionListener{
         pandemiaPanel = new JPanel();
         add(pandemiaPanel, BorderLayout.CENTER);
 
-        
-        ResourceBundle messages = ResourceBundle.getBundle("Messages", currentLocale);
-        setTitle(messages.getString("SymulacjaPandemii"));
 
-        
+        ResourceBundle messages = ResourceBundle.getBundle("Messages", currentLocale);
+        setTitle(messages.getString(TITLE_KEY));
+
+        virusPanel = new VirusPanel(this);
+        add(virusPanel, BorderLayout.EAST);
+
+        simulationPanel = new CFrame(numberOfPeople);
+        //pandemiaPanel.add(simulationPanel, BorderLayout.CENTER);
         menuBar = new JMenuBar();
-        
-        Menu = new JMenu(messages.getString("Menu"));
-        //Menu.setText(messages.getString("Menu"));
+
+        Menu = new JMenu(messages.getString(MENU_KEY));
         Menu.addActionListener(this);
 
-       // RUN = new JMenu((messages.getString("Run")));
-        //RUN.setText(messages.getString("Run"));
-       // RUN.addActionListener(this);
-        
-
-        
-        
-        SubMenu = new JMenu(messages.getString("Jezyk"));
-        //SubMenu.setText(messages.getString("Jezyk"));
+        SubMenu = new JMenu(messages.getString(LANGUAGE_KEY));
         SubMenu.addActionListener(this);
-        
-        newItem = new JMenuItem((messages.getString("Nowy")));
-        //newItem.setText(messages.getString("Nowy"));
-        newItem.addActionListener(this);
-        
-        saveItem = new JMenuItem(messages.getString("Zapisz"));
-        //saveItem.setText(messages.getString("Zapisz"));
-        saveItem.addActionListener(this);
-        
-        openItem = new JMenuItem(messages.getString("Otworz"));
-        //openItem.setText(messages.getString("Otworz"));
-        openItem.addActionListener(this);
-        
-        jezykItem = new JMenuItem(messages.getString("Jezyk"));
-        //jezykItem.setText(messages.getString("Jezyk"));
+
+        jezykItem = new JMenuItem(messages.getString(LANGUAGE_KEY));
         jezykItem.addActionListener(this);
-        
-        polskiItem = new JMenuItem(messages.getString("Polski"));
-        //polskiItem.setText(messages.getString("Polski"));
+
+        polskiItem = new JMenuItem(messages.getString(POLISH_KEY));
         polskiItem.addActionListener(this);
-        
-        angielskiItem = new JMenuItem(messages.getString("Angielski"));
-        //angielskiItem.setText(messages.getString("Angielski"));
+
+        angielskiItem = new JMenuItem(messages.getString(ENGLISH_KEY));
         angielskiItem.addActionListener(this);
+
+        changeBackgroundColorItem = new JMenuItem(messages.getString(CHANGE_BACKGROUND_COLOR_KEY));
+        changeBackgroundColorItem.addActionListener(this);
         
-        autorzyItem = new JMenuItem(messages.getString("Autorzy"));
-        //autorzyItem.setText(messages.getString("Autorzy"));
+        autorzyItem = new JMenuItem(messages.getString(AUTHORS_KEY));
         autorzyItem.addActionListener(this);
 
-        //runItem = new JMenuItem(messages.getString("Run")); // Changed this to JMenuItem
-        //runItem.addActionListener(this);
         
-        
-        Menu.add(newItem);
-        Menu.add(saveItem);
-        Menu.add(openItem);
+       
         SubMenu.add(polskiItem);
         SubMenu.add(angielskiItem);
         Menu.add(SubMenu);
+        //Menu.add(changeBackgroundColorItem);
         Menu.addSeparator();
         Menu.add(autorzyItem);
         menuBar.add(Menu);
-        //menuBar.add(RUN);
-        //Menu.add(runItem); // Added runItem to the menu bar
         setJMenuBar(menuBar);
 
-        virusPanel = new VirusPanel(this);
-        add(virusPanel, BorderLayout.EAST); // Virus panel added to EAST
         updateUILanguage();
-
-        // Removed the original colour buttons from here
     }
+
     
     void updateUILanguage() {
-    	  ResourceBundle messages = ResourceBundle.getBundle("Messages", currentLocale);
-    	  setTitle(messages.getString("SymulacjaPandemii"));
+        ResourceBundle messages = ResourceBundle.getBundle("Messages", currentLocale);
 
-    	  // Update menu items
-    	  Menu.setText(messages.getString("Menu"));
-    	  //RUN.setText(messages.getString("Run"));
-    	  //runItem.setText(messages.getString("Run")); // Update runItem text
-    	  SubMenu.setText(messages.getString("Jezyk"));
-    	  jezykItem.setText(messages.getString("Jezyk"));
-    	  newItem.setText(messages.getString("Nowy"));
-    	  saveItem.setText(messages.getString("Zapisz"));
-    	  openItem.setText(messages.getString("Otworz"));
-    	  polskiItem.setText(messages.getString("Polski"));
-    	  angielskiItem.setText(messages.getString("Angielski"));
-    	  autorzyItem.setText(messages.getString("Autorzy"));
-
-    	  // Update VirusPanel elements (assuming VirusPanel is a member variable)
-    	  virusPanel.pickVirusButton.setText(messages.getString("WybierzWirusa"));
-    	  virusPanel.colorVirusButton.setText(messages.getString("KolorWirusa") + ": ");
-    	  virusPanel.colorHealthyButton.setText(messages.getString("KolorZdrowego") + ": ");
-    	  virusPanel.colorImmuneButton.setText(messages.getString("KolorUodpornionego") + ": ");
-    	  virusPanel.colorDeadButton.setText(messages.getString("KolorZmarlego") + ": ");
-    	  virusPanel.listButton.setText(messages.getString("Lista") + ": ");
-    	  virusPanel.peopleLabel.setText(messages.getString("NumberLabel") + ": ");
-    	  virusPanel.parametersButton.setText(messages.getString("Parametry") + ": ");
-    	  
-    	}
+        setTitle(messages.getString(TITLE_KEY));
+        Menu.setText(messages.getString(MENU_KEY));
+        SubMenu.setText(messages.getString(LANGUAGE_KEY));
+        jezykItem.setText(messages.getString(LANGUAGE_KEY));
+        polskiItem.setText(messages.getString(POLISH_KEY));
+        angielskiItem.setText(messages.getString(ENGLISH_KEY));
+        autorzyItem.setText(messages.getString(AUTHORS_KEY));
+        changeBackgroundColorItem.setText(messages.getString(CHANGE_BACKGROUND_COLOR_KEY));
+        virusPanel.updateUILanguage(messages);
+    }
 
     
-    
-    
+    	
     
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == autorzyItem) {
-            JOptionPane.showMessageDialog(this, "Autorzy:\nMaciej Chaciński \nMichał Kostera", "Autorzy", JOptionPane.INFORMATION_MESSAGE);
-        } else if (e.getActionCommand().equals("New")) {
-            // ... new file handling ...
-        } else if (e.getActionCommand().equals("Save")) {
-            // ... save file handling ...
-        } else if (e.getActionCommand().equals("Open")) {
-            // ... open file handling ...
-        }
-/*        else if (e.getSource() == runItem) { // Check if the source is runItem
-            // Get the number of people from the virusPanel
-            int numberOfPeople = virusPanel.numberOfPeople;
+        	if(currentLocale.equals(new Locale("pl", "PL"))) {
+        		JOptionPane.showMessageDialog(this, "Autorzy:\nMaciej Chaciński \nMichał Kostera", "Autorzy", JOptionPane.INFORMATION_MESSAGE);
+        	}else {
+        		JOptionPane.showMessageDialog(this, "Authors :\nMaciej Chaciński \nMichał Kostera", "Authors", JOptionPane.INFORMATION_MESSAGE);
+   		 	}
             
-            // Initialize CFrame with the population size
-            CFrame frame = new CFrame(numberOfPeople);
-            
-            // Remove all components from pandemiaPanel
-            pandemiaPanel.removeAll();
-            
-            // Add the CFrame to pandemiaPanel
-            pandemiaPanel.add(frame);
-            
-            // Validate and repaint pandemiaPanel
-            pandemiaPanel.revalidate();
-            pandemiaPanel.repaint();
-        } */
-        else if (e.getSource() == polskiItem) {
-            currentLocale = new Locale("pl", "PL"); // Set Polish locale
+        } else if (e.getSource() == polskiItem) {
+            currentLocale = new Locale("pl", "PL");
             virusPanel.currentLocale = new Locale("pl", "PL");
             updateUILanguage();
-        } 
-        else if (e.getSource() == angielskiItem) {
-            currentLocale = Locale.US; // Set English locale (US for consistency)
-            virusPanel.currentLocale =  Locale.US;
+        } else if (e.getSource() == angielskiItem) {
+            currentLocale = Locale.US;
+            virusPanel.currentLocale = Locale.US;
             updateUILanguage();
-        } 
-        else if (e.getSource() instanceof JComponent && ((JComponent) e.getSource()).getParent() == virusPanel) {
-            // Delegate action handling to virusPanel if it's the source
+        } else if (e.getSource() == changeBackgroundColorItem) {
+            changeBackgroundColor();
+            //System.out.println("aaaaa");
+        }else if (e.getSource() instanceof JComponent && ((JComponent) e.getSource()).getParent() == virusPanel) {
             virusPanel.actionPerformed(e);
         }
     }
+
+    private void changeBackgroundColor() {
+        if (simulationPanel != null) {
+            Color selectedColor = JColorChooser.showDialog(this, "Wybierz kolor tła", simulationPanel.getBackground());
+            if (selectedColor != null) {
+                simulationPanel.setBackground(selectedColor);
+                //System.out.println("bbbbb");
+            }
+        } else {
+            JOptionPane.showMessageDialog(this, "Brak panelu symulacji do zmiany tła.", "Błąd", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+    
     public void setNumberOfPeople(int numberOfPeople) 
     {
         this.numberOfPeople = numberOfPeople;
-      }
+    }
+    
+    
     public int getNumberOfPeople() 
     {
         return numberOfPeople;
     }
     
     
-    
-    
 	public static void main(String[] args) {
     	Pandemia pandemia = new Pandemia();
         pandemia.setVisible(true);
 	}
-	
-
 }
